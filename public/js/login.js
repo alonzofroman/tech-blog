2166a28cbb9285624a59ae70ee8ce15db9cdd135
alonzofroman <alonzofroman@gmail.com>
2021-10-29 18:43:14 -0700

    Sign up script added

--- public/js/login.js
+++ public/js/login.js
@@ -22,4 +22,30 @@ const loginForm = async (event) => {
     }
 };
 
+const signUpForm = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector("#name-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                email, username, password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        }
+        else {
+            alert('Failed to signup');
+        }
+    }
+};
+
 document.querySelector(".login-form").addEventListener('submit', loginForm);
+document.querySelector('.signup-form').addEventListener('submit', signUpForm);
